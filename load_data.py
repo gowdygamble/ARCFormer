6688c7cdc84b98f5bb1c36841cef57ac4384f3b8
Paul <paulgamble49@gmail.com>
2023-03-11 22:20:19 -0800

    first train

--- load_data.py
+++ load_data.py
@@ -4,6 +4,8 @@ import random
 
 
 def load_data():
+    train_fraction = 0.9
+
     with open('x.pkl', 'rb') as f:
         x = pickle.load(f)
 
@@ -14,6 +16,17 @@ def load_data():
     random.shuffle(xy)
     x, y = zip(*xy)
 
-    print("data length:", len(x), len(y))
-    return x, y
+    n = int(train_fraction * len(x))
+    x_train, y_train = x[:n], y[:n]
+    x_val, y_val = x[n:], y[n:]
 
+    print("train:", len(x_train))
+    print("val:", len(x_val))
+    return x_train, y_train, x_val, y_val
+
+def pad_elements(elements, target_size):
+    pad_token = 16
+    for e in elements:
+        while len(e) < target_size:
+            e.append(pad_token)
+    return elements
--- train.py
+++ train.py
@@ -10,3 +10,89 @@
 # larger vocab -> shorter sequences
 # "hi there" char level encoding -> 8 tokens
 # "hi there" gpt2 encoding, 50k vocab size -> 3 tokens
+
+
+from load_data import load_data, pad_elements
+from gpt import ARCFormerModel
+
+import torch
+
+x_train, y_train, x_val, y_val = load_data()
+
+x_train = torch.tensor(pad_elements(x_train, 800), dtype=torch.long)
+x_val = torch.tensor(pad_elements(x_val, 800), dtype=torch.long)
+y_train = torch.tensor(pad_elements(y_train, 200), dtype=torch.long)
+y_val = torch.tensor(pad_elements(y_val, 200), dtype=torch.long)
+
+print(x_train.shape, y_train.shape)
+print(x_val.shape, y_val.shape)
+
+
+
+max_iters = 500
+eval_interval = 20
+eval_iters = 20
+learning_rate = 3e-4
+
+batch_size = 8
+device = 'cpu'
+
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    x = x_train if split == 'train' else x_val
+    y = y_train if split == 'train' else y_val
+    
+    ix = torch.randint(len(x), (batch_size,))
+
+    xt = torch.stack([x[i] for i in ix])
+    yt = torch.stack([y[i] for i in ix])
+    x, y = xt.to(device), yt.to(device)
+    return x, y
+
+
+
+
+
+
+
+
+
+
+model =ARCFormerModel()
+# print the number of parameters in the model
+print(sum(p.numel() for p in model.parameters())/1e6, 'M parameters')
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            #print('estimate loss', X.shape, Y.shape)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+for iter in range(max_iters):
+
+    # every once in a while evaluate the loss on train and val sets
+    if iter % eval_interval == 0 or iter == max_iters - 1:
+        losses = estimate_loss()
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+    # sample a batch of data
+    xb, yb = get_batch('train')
+
+    # evaluate the loss
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+
